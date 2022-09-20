5efb99d617acac638ab63d3862e7978469d23cc6
Eduardo Coelho <104034703+oducoelho@users.noreply.github.com>
2022-09-19 21:37:33 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { RepositoryList } from "./components/RepositoryList"
+
+import './App.scss'
+
+function App() {
+  return (
+    <>
+      <RepositoryList />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/RepositoryItem.jsx
@@ -0,0 +1,12 @@
+export const RepositoryItem = (props) => {
+  return (
+    <li>
+      <strong>{props.repository.name}</strong>
+      <p>{props.repository.description}</p>
+
+      <a href={props.repository.html_url}>
+        Acessar repositorio
+      </a>
+    </li>
+  )
+}
